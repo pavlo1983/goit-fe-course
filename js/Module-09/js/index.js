4a1066e61951808624afe42dc6a0da347e640d77
Pavlo Starynskyi <starynskyi@ukr.net>
2018-10-26 18:36:49 +0300

    26102018 work

--- js/Module-09/js/index.js
+++ js/Module-09/js/index.js
@@ -45,8 +45,9 @@
     в массив и добавление в ul.js-laps нового li с сохраненным временем в формате xx:xx.x
 */
 
-"use strict";
 
+
+/*
 const clockface = document.querySelector(".js-time");
 const startBtn = document.querySelector(".js-start");
 const lapBtn = document.querySelector(".js-take-lap");
@@ -97,7 +98,7 @@ class Stopwatch {
   }
 
   continue() {
-    this.timerId = setInterval(() => {}, 100)
+    this.timerId = setInterval(() => {}, 100);
   }
 
   startButtonOnClick() {
@@ -116,18 +117,17 @@ class Stopwatch {
 
   lapButtonOnClick() {
     if (startBtn.textContent === "Start") {
-     return;
+      return;
     } else if (startBtn.textContent === "Pause") {
       const timeLap = Date.now() - this.startTime;
-      list.insertAdjacentHTML("afterbegin", "<li> ${timeLap} </li>");
+      list.insertAdjacentHTML("afterbegin", `<li> ${timeLap} </li>`);
     } else if (startBtn.textContent === "Continue") {
       const timeLap = this.onPause - this.startTime;
-      list.insertAdjacentHTML("afterbegin", "<li> ${timeLap} </li>");
+      list.insertAdjacentHTML("afterbegin", `<li> ${timeLap} </li>`);
     }
   }
 
-
-  stop() {
+  stopButtonOnClick() {
     this.isActive = false;
     clearInterval(this.timerId);
     this.timerId = null;
@@ -144,13 +144,109 @@ const stopwatch = new Stopwatch({
   onTick: updateClockface
 });
 
-
-startBtn.addEventListener("click",stopwatch.startButtonOnClick.bind(stopwatch));
-lapBtn.addEventListener("click",stopwatch.lapButtonOnClick.bind(stopwatch));
-resetBtn.addEventListener("click", stopwatch.stop.bind(stopwatch));
+startBtn.addEventListener(
+  "click",
+  stopwatch.startButtonOnClick.bind(stopwatch)
+);
+lapBtn.addEventListener("click", stopwatch.lapButtonOnClick.bind(stopwatch));
+resetBtn.addEventListener("click", stopwatch.stopButtonOnClick.bind(stopwatch));
 
 function updateClockface({ min, sec, msec }) {
   clockface.textContent = `${min}:${sec}.${msec}`;
 }
+*/
+"use strict";
+
+
+
+class StopWatch {
+  constructor(parent = document) {
+    this.parent = parent;
+    this.startBtn = document.querySelector(".js-start");
+    this.lapBtn = document.querySelector(".js-take-lap");
+    this.resetBtn = document.querySelector(".js-reset");
+    this.list = document.querySelector(".js-laps");
+    this.clockface = document.querySelector(".js-time");
+    this.setIntervalId = null;
+    this.from = null;
+    this.to = null;
+  }
+
+  startBtnOnCLick() {
+    if (this.startBtn.textContent.toLowerCase() === "start") {
+      this.start();
+      this.startBtn.textContent = "Pause";
+    } else if (this.startBtn.textContent.toLowerCase() === "pause") {
+      this.startBtn.textContent = "Continue";
+      this.pause();
+    } else if (this.startBtn.textContent.toLowerCase() === "continue") {
+      this.startBtn.textContent = "Pause";
+      this.continue();
+    }
+  }
+  pause() {
+    this.to = Date.now();
+    clearInterval(this.setIntervalId);
+    this.clockface.textContent = this.toTimeString(this.to - this.from);
+  }
+
+  continue() {
+    this.from = Date.now() - (this.to - this.from);
+    this.step();
+  }
+
+  start() {
+    this.from = new Date();
+    this.step();
+  }
+
+  step() {
+    this.setIntervalId = setInterval(() => {
+      (this.clockface.textContent = this.toTimeString(Date.now() - this.from)),
+        1000 / 60;
+    });
+  }
+
+  lapBtnOnCLick() {
+    if (this.startBtn.textContent.toLowerCase() === "start") {
+     return;
+    } else if (this.startBtn.textContent.toLowerCase() === "pause") {
+      const time = this.toTimeString(Date.now() - this.from);
+      this.list.insertAdjacentHTML("afterbegin", `<li> ${time} </li>`)
+    } else if (this.startBtn.textContent.toLowerCase() === "continue") {
+      const time = this.toTimeString(this.to - this.from);
+      this.list.insertAdjacentHTML("afterbegin", `<li> ${time} </li>`)
+    }
+  }
+
+  resetBtnOnCLick() {
+    clearInterval(this.setIntervalId);
+    this.clockface.textContent = this.toTimeString(0);
+    this.startBtn.textContent = "Start";
+    this.list.innerHTML = '';
+  }
+
+  toTimeString(timeStamp) {
+    const date = new Date(timeStamp);
+    const time = date.toUTCString().slice(17, -4);
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const min = minutes >= 10 ? minutes : "0" + minutes;
+    const sec = seconds >= 10 ? seconds : "0" + seconds;
+    const msec = Number.parseInt(date.getMilliseconds() / 100);
+
+    return `${min}:${sec}.${msec}`;
+  }
+
+  run() {
+    this.startBtn.addEventListener("click", this.startBtnOnCLick.bind(this));
+    this.lapBtn.addEventListener('click', this.lapBtnOnCLick.bind(this));
+    this.resetBtn.addEventListener("click", this.resetBtnOnCLick.bind(this));
+  }
+}
+
+const stopWatch = new StopWatch(document.querySelector(".stopwatch"));
+stopWatch.run();
+
 
 
