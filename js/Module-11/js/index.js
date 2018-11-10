b8e677ba3675dddcb488d42e40f35f14f9efd5d0
Pavlo Starynskyi <starynskyi@ukr.net>
2018-11-11 00:27:53 +0200

    10112018 final m11

--- js/Module-11/js/index.js
+++ js/Module-11/js/index.js
@@ -117,125 +117,45 @@ const laptops = [
   }
 ];
 
-/*const source = document.querySelector("#notebook-card").innerHTML.trim();
-
-console.log(source);
-const template = Handlebars.compile(source);
-
-const markup = laptops.reduce((acc, laptop) => acc + template(laptop), "");
-//console.log(markup);
-
-//document.body.insertAdjacentHTML("afterend", markup);*/
-
-const filter = { size: [], color: [], release_date: [] };
-
 const submit = document.querySelector('button[type="submit"]');
 const reset = document.querySelector('button[type="reset"]');
 
-const form = document.querySelector("js-form");
 const inputs = document.querySelectorAll("input");
-const container = document.querySelector(".container");
+const list = document.querySelector(".js-list");
 
 submit.addEventListener("click", onCheck);
 reset.addEventListener("click", onReset);
-
-//const getSize = Array.from(document.querySelectorAll('input[name="size"]'));
-//const getColor = Array.from(document.querySelectorAll('input[name="color"]'));
-/*const getDate = Array.from(
-  document.querySelectorAll('input[name="release_date"]')
-);*/
-
-function onCheck(e) {
-  e.preventDefault();
-  if (!this.isActive) {
-    this.isActive = true;
-
-    const allInputs = [...inputs]
-      .filter(e => e.checked)
-      .map(e => (e.name === "color" ? e.value : +e.value));
-
-    const checked = laptops
-      .filter(e => allInputs.includes(e.size))
-      .filter(e => allInputs.includes(e.color))
-      .filter(e => allInputs.includes(e.release_date));
-
-    if (checked.length === 0) {
-      toDisplay(laptops);
-      return;
-    }
-    toDisplay(checked);
-
-    /*this.isActive = true;
-
-    const getAllSizes = getSize.filter(getSize => getSize.checked);
-    console.log(getAllSizes);
-
-    /* const sizes = getAllSizes.map(el => {
-      const arr = [];
-      
-        arr.push(Number(el.value));
-        console.log(arr);
-      
-      return arr;
-    });*/
-    /*function sizeValues() {
-      const arr = [];
-      for (let i = 0; i < getAllSizes.length; i += 1) {
-        arr.push(Number(getAllSizes[i].value));
-      }
-      return arr;
-    }
-
-    const sizes = sizeValues();
-    console.log(sizes);
-
- 
-   
-
-    const num = sizes.reduce((acc, val) => {
-      if (sizes.length > 0) {
-        return acc + 1
-      }
-    }
-    
-    );
-
-    console.log(num);
-    const color = getColor[0].value;
-    const date = Number(getDate[0].value);
-
-    function getBySize(num) {
-      return laptops.filter(laptop => laptop.size === num);
-    }
-
-    function getByColor(color) {
-      return laptops.filter(laptop => laptop.color === color);
-    }
-
-    function getByDate(date) {
-      return laptops.filter(laptop => laptop.release_date === date);
-    }
-
-    filter.size.push(getBySize(num));
-    filter.color.push(getByColor(color));
-    filter.release_date.push(getByDate(date));
-
-    console.log(filter);
-    //document.body.insertAdjacentHTML("afterend", markup);*/
-  }
-}
 
 function toDisplay(arr) {
   const source = document.querySelector("#laptop-card").innerHTML.trim();
   const template = Handlebars.compile(source);
   const markup = arr.reduce((acc, laptop) => acc + template(laptop), "");
-  container.innerHTML = markup;
+  list.innerHTML = markup;
 }
 
-toDisplay(laptops);
+function onCheck(e) {
+  e.preventDefault();
 
+  const allInputs = [...inputs]
+    .filter(el => el.checked)
+    .map(el => (el.name === "color" ? el.value : +el.value));
+
+  const checked = laptops
+    .filter(el => allInputs.includes(el.size))
+    .filter(el => allInputs.includes(el.color))
+    .filter(el => allInputs.includes(el.release_date));
+
+  if (checked.length === 0) {
+    toDisplay(laptops);
+    return;
+  }
+  toDisplay(checked);
+}
 
 function onReset(e) {
   e.preventDefault();
   toDisplay(laptops);
+  inputs.forEach(el => (el.checked = false));
 }
+
+toDisplay(laptops);
